40362a3de845a1e67f2c402888183b6ce8fe4d60
Malith-19 <malith.19@cse.mrt.ac.lk>
2025-10-10 18:38:53 +0530

    Reformat the code.

--- modules/charon-core/src/main/java/org/wso2/charon3/core/protocol/endpoints/BulkResourceManager.java
+++ modules/charon-core/src/main/java/org/wso2/charon3/core/protocol/endpoints/BulkResourceManager.java
@@ -94,7 +94,6 @@ public class BulkResourceManager extends AbstractResourceManager {
     public BulkResponseData processBulkData(BulkRequestData bulkRequestDataObject, UserManager userManager)
             throws CharonException, PayloadTooLargeException, BadRequestException {
 
-
         encoder = getEncoder();
         bulkRequestProcessor.setFailOnError(bulkRequestDataObject.getFailOnErrors());
         bulkRequestProcessor.setUserManager(userManager);
@@ -117,7 +116,6 @@ public class BulkResourceManager extends AbstractResourceManager {
 
         // Get bulk response data.
         return bulkRequestProcessor.processBulkRequests(bulkRequestDataObject);
-
     }
 
     public BulkRequestData getDecodeBulkRequest(String data) throws CharonException, BadRequestException {
@@ -140,7 +138,6 @@ public class BulkResourceManager extends AbstractResourceManager {
         // Create the final response.
         return new SCIMResponse(ResponseCodeConstants.CODE_OK, finalEncodedResponse, responseHeaders);
     }
-
 
     @Override
     public SCIMResponse get(String id, UserManager userManager, String attributes, String excludeAttributes) {
